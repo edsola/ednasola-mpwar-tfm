ce8fddb5801409ef654d11e2ed388efc527bb0f3
ednasola <94113146+edsola@users.noreply.github.com>
2022-07-28 16:12:03 +0200

    Render one ticket and menu template ticket

--- app/symfony/src/Controller/DashboardController.php
+++ app/symfony/src/Controller/DashboardController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Repository\TicketRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -18,4 +19,30 @@ class DashboardController extends AbstractController
             'tickets' => $tickets,
         ]);
     }
+
+    #[Route('/ticket/{id}', name: 'app_ticket')]
+    public function ShowTicket(Request $request, TicketRepository $ticketRepository): Response
+    {
+        $ticketID = $request->get('id');
+        $ticket = $ticketRepository->findOneBy(['id' => $ticketID]);
+
+        return $this->render('dashboard/ticket.html.twig', [
+            'ticket' => $ticket,
+        ]);
+    }
+
+    /*
+    #[Route('/ticket/{id}', name: 'app_ticket_status')]
+    public function ChangeTicketStatus(Request $request, TicketRepository $ticketRepository): Response
+    {
+        $ticketID = $request->get('id');
+        $ticket = $ticketRepository->findOneBy(['id' => $ticketID]);
+        $ticket->setStatusId()
+
+        return $this->render('dashboard/ticket.html.twig', [
+            'ticket' => $ticket,
+        ]);
+    }*/
+
+
 }
